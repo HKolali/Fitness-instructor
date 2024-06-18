7ab22f699d2d624ee2ca7333c5eafa630295c8e7
HKolali <kolali.hossein@gmail.com>
2024-06-18 17:15:19 +0330

    add main page and menu

---
+++ js/data.js
@@ -0,0 +1,19 @@
+
+let menu = [
+    {id:1, title:"صفحه اصلی", icon:"./image/menu-icon/home.png" , isActive:true, to:"/"},
+    {id:2, title:"خدمات", icon:"./image/menu-icon/service.png" , isActive:false, to:"/services"},
+    {id:3, title:"مکمل‌های ورزشی", icon:"./image/menu-icon/herbal.png" , isActive:false, to:"/supplement"},
+    {id:4, title:"آموزش حرکات", icon:"./image/menu-icon/movement.png" , isActive:false, to:"/movement"},
+    {id:5, title:"تغذیه", icon:"./image/menu-icon/diet.png" , isActive:false, to:"/nutrition"},
+    {id:6, title:"مقالات", icon:"./image/menu-icon/news.png" , isActive:false, to:"/articles"},
+    {id:7, title:"ارتباط با من", icon:"./image/menu-icon/contact.png" , isActive:false, to:"/contact-me"}
+]
+
+let service = [
+    {id:1, title:"برنامه تمرینی", desc:"برنامه تمرینی با جدیدترین سیستم های تمرینی", image:"./image/service/program.png" , alt:"برنامه تمرینی"},
+    {id:2, title:"برنامه غذایی", desc:"ارائه برنامه غذایی مطابق با هدف شما", image:"./image/service/healthy-food.png" , alt:"برنامه غذایی"},
+    {id:3, title:"تمرین در منزل", desc:"برنامه تمرین در منزل متناسب با امکانات شما", image:"./image/service/stay-at-home.png" , alt:"تمرین در منزل"},
+    {id:4, title:"مشاوره ورزشی", desc:"مشاوره ورزشی با مربی بدنسازی", image:"./image/service/conversation.png" , alt:"مشاوره ورزشی"},
+]
+
+export {menu, service};
